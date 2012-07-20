c7cdc6ecfe5c2431e57eb34b96e4f48e84ac8e59
Gerd Zschaler <gerd@gzschaler.de>
2012-07-20 13:08:39 +0200

    Error handling

--- src/bornholdt.cpp
+++ src/bornholdt.cpp
@@ -1,9 +1,19 @@
+#include <exception>
+#include <iostream>
+
 #include "BornholdtApp.h"
 
 int main(int argc, char **argv)
 {
-	BornholdtApp a;
-	a.parseCommandLine(argc, argv);
-	return a.exec();
+	try
+	{
+		BornholdtApp a;
+		a.parseCommandLine(argc, argv);
+		return a.exec();
+	} catch (std::exception& e)
+	{
+		std::cerr << e.what() << "\n";
+		return 1;
+	}
 }
 
