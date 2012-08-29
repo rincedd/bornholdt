f7cbcb297d1b299af5ba97d0072f3abf3426cc31
Gerd Zschaler <gerd@gzschaler.de>
2012-08-29 13:45:27 +0200

    Fraction of synchronous spikes as local order parameter

--- src/CorrelationAccumulator.h
+++ src/CorrelationAccumulator.h
@@ -1,22 +1,15 @@
 #ifndef CORRELATIONACCUMULATOR_H_
 #define CORRELATIONACCUMULATOR_H_
 
-#include <boost/accumulators/accumulators.hpp>
-#include <boost/accumulators/statistics/stats.hpp>
-#include <boost/accumulators/statistics/mean.hpp>
-
-namespace ba = boost::accumulators;
-
 class CorrelationAccumulator
 {
-	typedef ba::accumulator_set<double, ba::stats<ba::tag::mean> > mean_acc_t;
 public:
 	CorrelationAccumulator() :
-			state_i_(0), state_j_(0)
+			state_i_(0), state_j_(0), productSum_(0), sumSum_(0)
 	{
 	}
 	CorrelationAccumulator(const short& state_i, const short& state_j) :
-			state_i_(&state_i), state_j_(&state_j)
+			state_i_(&state_i), state_j_(&state_j), productSum_(0), sumSum_(0)
 	{
 	}
 
@@ -29,16 +22,22 @@ public:
 	void update()
 	{
 		if ((state_i_ != 0) && (state_j_ != 0))
-			acc_(*state_i_ + *state_j_);
+		{
+			productSum_ += *state_i_ * *state_j_;
+			sumSum_ += *state_i_ + *state_j_;
+		}
 	}
 	double mean() const
 	{
-		return ba::mean(acc_) / 2.0;
+		if (sumSum_ > 0)
+			return 2 * productSum_ / sumSum_;
+		else
+			return 0;
 	}
 private:
 	const short* state_i_;
 	const short* state_j_;
-	mean_acc_t acc_;
+	double productSum_, sumSum_;
 };
 
 #endif /* CORRELATIONACCUMULATOR_H_ */
